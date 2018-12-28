e1c5d20db80fdbcbe1acb2ede7aaddb68e5f015d
Ronney Vargas <ronneyvargas@gmail.com>
2018-12-28 17:38:15 -0600

    Added javascript for dice and scores to be set at 0 when game is started.

--- app.js
+++ app.js
@@ -9,9 +9,38 @@ GAME RULES:
 
 */
 // Setting score variables
-
-var scores, roundScore;
+var scores, roundScore, activePlayer;
 
 scores = [0, 0];
-roundScore = 0; 
+roundScore = 0;
+activePlayer = 1;
+
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    // 1. need a random number
+    var dice = Math.floor(Math.random() * 6) + 1;
+    // 2. Display the result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice +'.png';
+    
+
+    // 3. Update the round score only IF the rolled number was NOT 1
+});
+
+
+// textContent can only add to plain-text and NO html
+// Setting a value
+// document.querySelector('#current-' + activePlayer).textContent = dice;
+// innerHTML can add html tags
+// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+
+// Getting a value
+// var x = document.querySelector('#score-0').textContent;
 
