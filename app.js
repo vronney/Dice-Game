8fae47f2fe36aa97d61c9695ed2f69c34a6adaae
Ronney Vargas <ronneyvargas@gmail.com>
2018-12-29 14:11:31 -0600

    Added DOM to the new btn and hold btn.

--- app.js
+++ app.js
@@ -9,19 +9,27 @@ GAME RULES:
 
 */
 // Setting score variables
+
 var scores, roundScore, activePlayer;
 
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 1;
+init();
 
-document.querySelector('.dice').style.display = 'none';
+function init() {
+    scores = [0, 0];
+    roundScore = 0;
+    activePlayer = 0;
+    // Initial setting - all are cleared
+    document.querySelector('.dice').style.display = 'none';
 
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+};
 
+// Roll Dice button
 document.querySelector('.btn-roll').addEventListener('click', function() {
     // 1. need a random number
     var dice = Math.floor(Math.random() * 6) + 1;
@@ -29,18 +37,52 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     var diceDOM = document.querySelector('.dice');
     diceDOM.style.display = 'block';
     diceDOM.src = 'dice-' + dice +'.png';
-    
-
     // 3. Update the round score only IF the rolled number was NOT 1
+    if (dice !== 1) {
+        // Add score
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+        // Next player
+        nextPlayer();
+    }
 });
 
+// New Game <button class="btn">
+document.querySelector('.btn-new').addEventListener('click', init);
 
-// textContent can only add to plain-text and NO html
-// Setting a value
-// document.querySelector('#current-' + activePlayer).textContent = dice;
-// innerHTML can add html tags
-// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+// Hold current score <button class="btn">
+document.querySelector('.btn-hold').addEventListener('click', function() {
+    // Add current score to global 
+    scores[activePlayer] += roundScore;
 
-// Getting a value
-// var x = document.querySelector('#score-0').textContent;
+    // Update the UI 
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+    // Check if player has won
+    if (scores[activePlayer] >= 20) {
+        document.querySelector('#name-' + activePlayer).textContent = "You win!";
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+    } else {
+        nextPlayer();
+    }
+});;    
+
+
+function nextPlayer() {
+    // Next Player
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    document.querySelector('.dice').style.display = 'none';
+}
+
 
